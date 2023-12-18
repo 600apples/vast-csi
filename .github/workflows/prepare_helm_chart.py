02e20f5351232dd5a68e5ccc452af7c49448fe90
Volodymyr Boiko <volodymyr.boiko@vastdata.com>
2023-12-18 15:51:14 +0200

    ls

--- .github/workflows/prepare_helm_chart.py
+++ .github/workflows/prepare_helm_chart.py
@@ -1,1 +1,20 @@
-print("Script!!!!!!!!!!!!!!")
+import os
+from pathlib import Path
+
+ROOT = Path.cwd()
+BRANCH = os.environ["GITHUB_REF_NAME"]
+RUN_ID = os.environ["GITHUB_RUN_ID"]
+VERSION = ROOT.joinpath("version.txt").read_text().strip()
+
+
+if __name__ == '__main__':
+    print(f"::set-output name=branch::{BRANCH}")
+    print(f"::set-output name=run_id::{RUN_ID}")
+    print(f"::set-output name=version::{VERSION}")
+
+
+
+
+
+
+
