ce5fac3313c4ebf0af9ec132eab22c9101ba386e
Volodymyr <600apples@gmail.com>
2023-12-18 19:18:58 +0200

    package

--- .github/workflows/prepare_helm_chart.py
+++ .github/workflows/prepare_helm_chart.py
@@ -16,9 +16,6 @@ if __name__ == '__main__':
     pages_branch = "gh_pages_beta" if is_beta else "gh-pages"
     packages_with_index = "true" if is_beta else "false"
     skip_upload = "true" if is_beta else "false"
-
-
-
     version = f"{VERSION}-beta.{SHA}" if is_beta else f"{VERSION}-{SHA}"
 
     for line in fileinput.input(CHART, inplace=True):
@@ -28,8 +25,8 @@ if __name__ == '__main__':
 
     ROOT.joinpath("releaser-config.yaml").open("w").write(
         f"""
-pages_branch: {pages_branch}
-release_name_template: {release_name_template}
-packages_with_index: {packages_with_index}
-skip_upload: {skip_upload}
+pages-branch: {pages_branch}
+release-name-template: {release_name_template}
+packages-with-index: {packages_with_index}
+skip-upload: {skip_upload}
 """)
