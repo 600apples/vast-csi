ed74ddfaa050865b2e5718e54ae3cc03c9aa3310
Volodymyr Boiko <volodymyr.boiko@vastdata.com>
2023-12-18 16:01:55 +0200

    ls

--- .github/workflows/prepare_helm_chart.py
+++ .github/workflows/prepare_helm_chart.py
@@ -3,14 +3,19 @@ from pathlib import Path
 
 ROOT = Path.cwd()
 BRANCH = os.environ["GITHUB_REF_NAME"]
-RUN_ID = os.environ["GITHUB_RUN_ID"]
-VERSION = ROOT.joinpath("version.txt").read_text().strip()
+RUN_ID = os.environ["GITHUB_RUN_ID"][:5]
+VERSION = ROOT.joinpath("version.txt").read_text().strip().lstrip("v")
 
 
 if __name__ == '__main__':
-    print(f"branch {BRANCH}")
-    print(f"name=run_id {RUN_ID}")
-    print(f"name=version {VERSION}")
+    is_beta = BRANCH.startswith("beta")
+    if is_beta:
+        version = f"{VERSION}-beta.{RUN_ID}"
+    else:
+        version = f"{VERSION}.{RUN_ID}"
+
+
+    print(f"version: {version}")
 
 
 
